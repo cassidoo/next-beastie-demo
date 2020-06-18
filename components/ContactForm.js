2738ee16fd21a3e2d6f181ac9c9508e4ae57fc3b
cassidoo <cassidoo@gmail.com>
2020-06-18 09:42:06 -0700

    Add success page

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,5 +1,5 @@
 export default function ContactForm() {
-  return <form name="contact" action="/" method="POST" data-netlify="true">
+  return <form name="contact" action="/success" method="POST" data-netlify="true">
     <p>
       <label htmlFor="youremail">Email: </label>
       <input type="email" id="youremail" />
---
+++ pages/success.js
@@ -0,0 +1,65 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Footer from '@components/Footer'
+import ContactForm from '@components/ContactForm'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Form submitted!!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        Hooray your form was submitted!
+        <Link href="/">
+          <a>Go back home</a>
+        </Link>
+      </main>
+
+      <Footer />
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
